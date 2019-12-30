1d8fbc29adea69ee9a649749fb08b7a9a0b437ce
MateosDev <35548023+MateosDev@users.noreply.github.com>
2019-12-30 16:21:05 -0600

    Testing functionality on bamazonCustomer.js

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,4 +1,5 @@
-// Global Requirements
+// Global Requirements 
+
 require("dotenv").config();
 var keys = require("./keys.js");
 var mysql = require("mysql");
@@ -8,28 +9,130 @@ var stockCheck;
 // console.log(keys.password);
 var connection = mysql.createConnection(keys);
 
+//Initiate the Database Connection
 
-//Connection 
 connection.connect(function(err) {
-    if (err) throw err;
-    initConnection();
+  if (err) throw err;
+  initConnection();
+});
+
+//Items For Sale List/Table
+
+function initConnection() {
+  connection.query("SELECT id, product_name, price FROM products", function(
+    err,
+    res
+  ) {
+   if (err) {
+      console.log(err);
+    }
+    for (let i = 0; i < res.length; i++) {
+      res[i].price = res[i].price.toFixed(2);
+    }
+
+    console.table(res);
+    // console.table(transformed);
+    purchaseItem();
   });
-  
-  //Items for Sale Table
-  function initConnection() {
-    connection.query("SELECT id, product_name, price FROM products", function(
-      err,
-      res
-    ) {
-      if (err) {
-        console.log(err);
+}
+
+// starting the product purchasing
+function purchaseItem() {
+  inquirer
+    .prompt([
+      {
+        type: "number",
+        name: "id",
+        message: "Type the ID of the product you want to purchase."
+      },
+      {
+        type: "number",
+        name: "stock_quantity",
+        message: "Type the amount you would like to purchase."
       }
-      for (let i = 0; i < res.length; i++) {
-        res[i].price = res[i].price.toFixed(2);
-      }
-  
-      console.table(res);
-      // console.table(transformed);
-      startBuy();
+    ])
+    .then(function(answer) {
+      connection.query(
+        "SELECT stock_quantity FROM products WHERE id = ? ",
+        [answer.id],
+        function(err, res) {
+          if (err) {
+            console.log(err);
+          }
+          stockCheck = res[0].stock_quantity;
+          if (stockCheck < answer.stock_quantity) {
+            stockChecking();
+          } else {
+            connection.query(
+              "UPDATE products SET stock_quantity = stock_quantity - ? WHERE id = ? ",
+              [answer.stock_quantity, answer.id],
+              function(err) {
+                if (err) {
+                  console.log(err);
+                }
+                salePrice(answer.id, answer.stock_quantity);
+              }
+            );
+          }
+        }
+      );
     });
-  }
+}
+
+//trying to make a reciept
+function salePrice(id, amount) {
+  connection.query("SELECT price FROM products WHERE id = ? ", [id], function(
+    err,
+    res
+  ) {
+    if (err) {
+      console.log(err);
+    }
+    console.log(
+      //reciept for amount of items and price?
+      "Thank you for your choosing Bamazon. The total " +
+        amount +
+        "X item ID:" +
+        id +
+        " is $" +
+        (res[0].price * amount).toFixed(2)
+    );
+    shopAgain();
+  });
+}
+
+function stockChecking() {
+  inquirer
+    .prompt({
+      name: "NoStock",
+      type: "list",
+      message: "Sorry we do not have enough of that item in stock.",
+      choices: ["Continue Shopping?", "Leave"]
+    })
+    .then(function(answer) {
+      if (answer.NoStock === "Continue Shopping?") {
+        purchaseItem();
+      }
+      if (answer.NoStock === "Leave") {
+        connection.end();
+      }
+    });
+}
+
+function shopAgain() {
+  inquirer
+    .prompt({
+      name: "shopAgain",
+      type: "list",
+      message: "Would you like to purchase anything else?",
+      choices: ["Continue Shopping", "Leave"]
+    })
+    .then(function(answer) {
+      if (answer.shopAgain === "Continue Shopping") {
+        purchaseItem();
+      }
+      if (answer.shopAgain === "Leave") {
+        connection.end();
+      }
+    });
+}
