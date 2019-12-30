d7dd1e550983d0a9f74cd315213d9b32c95af558
MateosDev <35548023+MateosDev@users.noreply.github.com>
2019-12-30 08:14:13 -0600

    Originally had DB and Produts on one file, changed that so that products have their own place in the seed file. This way anyone cloning the repo or running the app can create and run the DB, but change/add products later

---
+++ bamazon_db.sql
@@ -0,0 +1,35 @@
+/*
+    BAMAZON - DATABASE 
+    https://github.com/MateosDev/Bamazon
+*/
+
+-- If DB Exists, use DROP DATABASE command
+
+DROP DATABASE IF EXISTS bamazon_db;
+
+-- Creating the DB
+
+CREATE DATABASE bamazon_db;
+
+--Using/Selecting the DB to use. 
+
+USE bamazon_db;
+
+-- Create Table in DB
+
+CREATE TABLE products
+(
+    id INT NOT NULL
+    AUTO_INCREMENT,
+  product_name VARCHAR
+    (45) NULL,
+  department_name VARCHAR
+    (45) NULL,
+    price
+  DECIMAL
+    (10,2) NOT NULL,
+    stock_quantity INT
+  NULL,
+  PRIMARY KEY
+    (id)
+);
